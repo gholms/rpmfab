f2b4ce75c4c3ebe453095e71acf1c15c1f2b272c
Garrett Holmstrom <gholms@devzero.com>
2015-11-24 17:04:01 -0800

    Call mock with -v

--- build-arch.py
+++ build-arch.py
@@ -18,7 +18,7 @@ __version__ = '0.2'
 
 def build_arch(srpm, chroot, resultdir, mock_opts=None):
     logging.info('Building RPMs in %s using chroot %s', resultdir, chroot)
-    args = ['/usr/bin/mock', '-r', chroot, '--resultdir', resultdir]
+    args = ['/usr/bin/mock', '-v', '-r', chroot, '--resultdir', resultdir]
     args.extend(mock_opts or [])
     args.extend(['--rebuild', srpm])
     logging.debug("Executing ``%s''", ' '.join(args))
--- build-srpm-from-scm.py
+++ build-srpm-from-scm.py
@@ -310,7 +310,7 @@ class SRPMBuilder(object):
     def build_srpm(self, resultdir):
         logging.info('Building source RPM in %s using chroot %s', resultdir,
                      self.chroot)
-        args = ['/usr/bin/mock', '-r', self.chroot, '--resultdir', resultdir]
+        args = ['/usr/bin/mock', '-v', '-r', self.chroot, '--resultdir', resultdir]
         args.extend(self.mock_opts or [])
         args.extend(['--buildsrpm', '--spec', self.specfile, '--sources',
                      os.getcwd()])
